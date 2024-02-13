d197cb291de9532942fb788e31cb7d1043a30b4a
Bruxar <larancibia@utem.cl>
2024-02-12 21:57:38 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './app.scss'
+
+function App() {
+
+  return (
+    <section className='contenedor-sass-react'>
+      <div className='sass-react'>
+        <img src="/sass.png" alt="sass-icon" />
+        <img src="/react.png" alt="react-icon" />
+      </div>
+    </section>
+  )
+}
+
+export default App
